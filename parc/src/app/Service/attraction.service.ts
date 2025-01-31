b678842fbcbb31a9dede68f9343418040f5ff956
Omadole <105947438+Omadole@users.noreply.github.com>
2025-01-31 17:31:28 +0100

    Correction bug affiche les attractions visibles

--- parc/src/app/Service/attraction.service.ts
+++ parc/src/app/Service/attraction.service.ts
@@ -19,6 +19,14 @@ export class AttractionService {
     return data as Observable<AttractionInterface[]>;
   }
 
+  public getVisibleAttraction() : Observable<AttractionInterface[]> {
+    const url = "http://127.0.0.1:5000/attraction/visible"
+    const data = this.dataService.getData(url);
+    return data as Observable<AttractionInterface[]>;
+  }
+
+
+
   public postAttraction(attraction: AttractionInterface): Observable<MessageInterface> {
     const url = "http://127.0.0.1:5000/attraction";
     const data = this.dataService.postData(url, attraction);
